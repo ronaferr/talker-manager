14234ce6c6d5b68715e5116ea12f1fd78efcd73f
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-08-05 16:34:43 -0300

    endponit de alteração, remoção

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs/promises');
 const crypto = require('crypto');
 const validationPassword = require('./middlewares/validationPassword');
 const validationUser = require('./middlewares/validationUser');
+const validationToken = require('./middlewares/validationToken');
+const validationName = require('./middlewares/validationName');
+const validationAge = require('./middlewares/validationAge');
+const validationTalk = require('./middlewares/validationTalk');
+const validationTalkRate = require('./middlewares/validationTalkRate');
 
 const talkerList = async () => {
-  const talkers = await JSON.parse(fs.readFileSync('./talker.json', 'UTF-8'));
-  return talkers;
+  const list = await fs.readFile('./talker.json', 'UTF-8');
+  return JSON.parse(list);
 };
+
+const setTalker = (newTalker) => fs.writeFile('./talker.json', JSON.stringify(newTalker));
 
 function generateToken() {
   return crypto.randomBytes(8).toString('hex');
@@ -25,10 +32,15 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (_req, res) => {
+app.post('/login', validationPassword, validationUser, (req, res) => {
+  const newToken = generateToken();
+  return res.status(HTTP_OK_STATUS).json({ token: newToken });
+});
+
+app.get('/talker', async (_req, res) => {
   try {
-    const talkers = JSON.parse(fs.readFileSync('./talker.json', 'UTF-8'));
-    return res.status(HTTP_OK_STATUS).send(talkers);
+    const talkers = await talkerList() || [];
+    return res.status(HTTP_OK_STATUS).json(talkers);
   } catch (e) {
     return res.status(500).send('Algo deu errado');
   }
@@ -48,9 +60,58 @@ app.get('/talker/:id', async (req, res) => {
   }
 });
 
-app.post('/login', validationPassword, validationUser, (req, res) => {
-  const newToken = generateToken();
-  return res.status(HTTP_OK_STATUS).json({ token: newToken });
+app.post('/talker',
+validationToken,
+validationName,
+validationAge,
+validationTalk,
+validationTalkRate, async (req, res) => {
+  try {
+  const talkersList = await talkerList();
+  const id = talkersList.length + 1;
+  const { name, age, talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const newTlk = { name, age, id, talk: { watchedAt, rate } };
+  talkersList.push(newTlk);
+  setTalker(talkersList);
+  return res.status(201).json(newTlk);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+app.put('/talker/:id', validationToken, validationName, validationAge, validationTalk,
+validationTalkRate, async (req, res) => {
+  try {
+  const talkersList = await talkerList();
+  const { id } = req.params;
+  const talkerIndex = talkersList.findIndex((t) => t.id === Number(id));
+  talkersList[talkerIndex] = {
+    ...talkersList[talkerIndex],
+    ...req.body,
+  };
+  await setTalker(talkersList);
+  return res.status(200).send(talkersList[talkerIndex]);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+app.delete('/talker/:id',
+validationToken, async (req, res) => {
+  try {
+  const talkersList = await talkerList();
+  const { id } = req.params;
+  
+  const talkerIndex = talkersList.findIndex((t) => t.id === Number(id));
+
+  talkersList.splice(talkerIndex, 1);
+  await setTalker(talkersList);
+
+  res.status(204).end();
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 });
 
 app.listen(PORT, () => {
---
+++ middlewares/validationTalk.js
@@ -0,0 +1,16 @@
+module.exports = (req, res, next) => {
+  const { talk } = req.body;
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!talk) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+  if (!talk.watchedAt) {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!dateRegex.test(talk.watchedAt)) {
+    return res.status(400).json(
+      { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' },
+    );
+  }
+  next();
+};
