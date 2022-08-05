fde25a096616b64791c11a133c1fd388edc918d4
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-08-05 07:45:06 -0300

    criação de endpoint GET /talker/id

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+const talkerList = async () => {
+  const talkers = await JSON.parse(fs.readFileSync('./talker.json', 'UTF-8'));
+  return talkers;
+};
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -19,6 +24,20 @@ app.get('/talker', (_req, res) => {
     return res.status(HTTP_OK_STATUS).send(talkers);
   } catch (e) {
     return res.status(500).send('Algo deu errado');
+  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkers = await talkerList();
+    const talker = talkers.find((tlk) => tlk.id === Number(id));
+  
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+    return res.status(HTTP_OK_STATUS).json(talker);
+  } catch (e) {
+    return res.status(500).send(e.message);
   }
 });
 
